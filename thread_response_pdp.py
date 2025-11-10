b4e48bd9ecfe045938a7b6ff0ec2fd614693234e
AjayChauhanActowiz <ajaychauhan.actowiz@gmail.com>
2025-11-10 15:58:08 +0530

    Refactor thread_response_pdp.py for clarity and updates

--- thread_response_pdp.py
+++ thread_response_pdp.py
@@ -25,7 +25,7 @@ def genetrate_header():
     return headers
 # import urllib3
 # urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-# token = "f42a5b59aec3467e97a8794c611c436b91589634343"
+# token = "token"
 # proxyModeUrl = "http://{}:@proxy.scrape.do:8080".format(token)
 # proxies = {
 #     "http": proxyModeUrl,
@@ -93,5 +93,6 @@ file_name = 'BinDawood_app_pdp_feasibility_test'
 df = pd.DataFrame(results)
 df.to_excel(f'{file_name}.xlsx', index=False)
 print(f"Results saved to {file_name}.xlsx")
+
 
 
